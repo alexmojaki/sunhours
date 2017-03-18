6e0bc3a6a2c3b0c664aff7987d41b904f65f0b78
Alex Hall <alex.mojaki@gmail.com>
2017-03-18 21:27:02 +0200

    Add README, LICENSE, and comments in package.sh

---
+++ README.md
@@ -0,0 +1,6 @@
+# SunHours
+
+SunHours, a free plugin for SketchUp, lets you visualise how sunlight interacts with your building, allowing for accurate consideration of sunlight early in the design process.
+
+Website: http://www.sunhoursplugin.com/
+Extension Warehouse: https://extensions.sketchup.com/en/node/1991
--- package.sh
+++ package.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Creates .zip and .rbz files for distribution
+
 set -eux
 
 rm *.rbz *.zip
@@ -10,6 +12,18 @@ zip=${name}.zip
 rbz=${name}.rbz
 
 find . | grep .DS_Store | xargs rm
+
+# This .rb file and folder are the actual plugin files that are ultimately
+# placed in SketchUp's plugins folder and run.
 zip -r -X ${zip} AlexHall_SunHours AlexHall_SunHours.rb
+
+# The .rbz archive is uploaded to the Extension Warehouse.
 cp ${zip} ${rbz}
+
+# Finally this .zip can be downloaded from http://sunhoursplugin.com/index.php#download
+# It contains the offline documentation plus both the .zip and .rbz,
+# which are identical apart from the file extension.
+# This is so that users can easily use one or the other without knowing how to rename the
+# extension. The .rbz can be installed directly by SketchUp while the .zip is for manual
+# copying to the plugins folder.
 zip -r -X ${name}_SolidGreen.zip ${zip} ${rbz} SunHours_Documentation.pdf
