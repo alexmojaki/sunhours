616914ec9a82539b6e8e0a93495dcdcdf585f28d
Alex Hall <alex.mojaki@gmail.com>
2018-01-14 14:17:52 +0200

    Add link_install.sh

---
+++ link_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+plugins="$HOME/Library/Application Support/SketchUp 2018/SketchUp/Plugins"
+
+ln -s "`pwd`/AlexHall_SunHours.rb" "${plugins}/AlexHall_SunHours.rb"
+ln -s "`pwd`/AlexHall_SunHours" "${plugins}/AlexHall_SunHours"
